52c3ad7f1a337cba55a45f9cc9ea1ae78b539536
PersianDavinci <persiandavinci@gmail.com>
2023-10-27 23:32:10 +0330

    new db

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -13,22 +13,29 @@ export async function POST(req: Request) {
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env.local"
     );
   }
+  // Get the headers
   const headerPayload = headers();
   const svix_id = headerPayload.get("svix-id");
-  const svix_timestamp = headerPayload.get("svix_timestamp");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
   const svix_signature = headerPayload.get("svix-signature");
 
+  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", { status: 400 });
+    return new Response("Error occured -- no svix headers", {
+      status: 400,
+    });
   }
 
+  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
+  // Create a new SVIX instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
 
+  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       "svix-id": svix_id,
@@ -36,12 +43,17 @@ export async function POST(req: Request) {
       "svix-signature": svix_signature,
     }) as WebhookEvent;
   } catch (err) {
-    console.error("error verifying webhook", err);
-    return new Response("error occured", { status: 400 });
+    console.error("Error verifying webhook:", err);
+    return new Response("Error occured", {
+      status: 400,
+    });
   }
 
+  // Get the ID and type
+  const { id } = evt.data;
   const eventType = evt.type;
-  console.log({ eventType });
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log("Webhook body:", body);
 
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
---
+++ lib/actions/tag.actions.ts
@@ -0,0 +1,33 @@
+"use server";
+
+import User from "@/database/user.model";
+import { connectToDatabase } from "../mongoose";
+import { GetAllTagsParams, GetTopInteractedTagsParams } from "./shared.types";
+import Tag from "@/database/tag.model";
+
+export async function GetTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    connectToDatabase();
+
+    const { userId } = params;
+    const user = await User.findById(userId);
+    if (!user) throw new Error("User not found");
+
+    return [{ _id: "1", name: "tag1" }];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+    const tags = await Tag.find({});
+
+    return { tags };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
