1797b5d74054ece9919d5d42582fc50b25a66e49
PersianDavinci <persiandavinci@gmail.com>
2023-10-27 15:31:58 +0330

    newmain

---
+++ lib/actions/question.action.ts
@@ -0,0 +1,53 @@
+"use server";
+
+import { connectToDatabase } from "../mongoose";
+import Question from "@/database/question.model";
+import Tag from "@/database/tag.model";
+import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import User from "@/database/user.model";
+import { revalidatePath } from "next/cache";
+
+export async function getQuestion(params: GetQuestionsParams) {
+  try {
+    connectToDatabase();
+
+    const questions = await Question.find({})
+      .populate({ path: "tags", model: Tag })
+      .populate({ path: "author", model: User })
+      .sort({ createdAt: -1 });
+
+    return { questions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createQuestion(params: CreateQuestionParams) {
+  try {
+    connectToDatabase();
+
+    const { title, content, tags, author, path } = params;
+
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+    const tagDocuments = [];
+
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag, $push: { question: question._id } } },
+        { upsert: true, new: true }
+      );
+      tagDocuments.push(existingTag._id);
+    }
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } },
+    });
+
+    revalidatePath(path);
+  } catch (error) {}
+}
